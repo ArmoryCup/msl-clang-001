9038ef63ce6054aad85ea8e7750f79cb57c64a82
Kamil <farsh03@gmail.com>
2018-01-21 22:29:40 -0700

    Third commit

--- main.c
+++ main.c
@@ -3,36 +3,30 @@
 #include <string.h>
 
 typedef struct node {
-    int count;
+    unsigned int count;
     const char *word;
     struct node *left;
     struct node *right;
 } Node;
 
-Node *root = 0;
+static const char SPACE = 32;
 
 
 Node *createLeaf(char *word) {
     struct node *leaf = (Node *) malloc(sizeof(Node));
     leaf->word = malloc(sizeof(word));
     strcpy(leaf->word, word);
+    leaf->count = 1;
     leaf->left = NULL;
     leaf->right = NULL;
     return leaf;
 }
 
-int buildTree(char *word) {
-    if (root != 0)
-        return 1;
-    else {
-        root = createLeaf(word);
-        return 0;
+
+void insertWord(Node *leaf, char *word) {
+    if (searchWord(leaf, word)) {
+        return;
     }
-
-
-}
-
-void insertWord(Node *leaf, const char *word) {
     if (leaf == NULL)
         leaf = createLeaf(word);
 
@@ -41,7 +35,8 @@ void insertWord(Node *leaf, const char *word) {
             insertWord(leaf->left, word);
         else
             leaf->left = createLeaf(word);
-    } else if (word > leaf->word) {
+
+    } else if (strcmp(word, leaf->word) > 0) {
         if (leaf->right != NULL)
             insertWord(leaf->right, word);
         else
@@ -49,49 +44,97 @@ void insertWord(Node *leaf, const char *word) {
     }
 }
 
+int searchWord(Node *node1, char *word) {
+    if (node1 == 0)
+        return 0;
 
-int main(int argc, char **argv) {
+    if (strcmp(word, node1->word) == 0) {
+        ++node1->count;
+        return 1;
+    } else if (strcmp(word, node1->word) < 0)
+        searchWord(node1->left, word);
 
-//    Node *root = createLeaf("one");
+    else if (strcmp(word, node1->word) > 0)
+        searchWord(node1->right, word);
+}
 
-//    insertWord(root, "go");
-//    insertWord(root, "two");
-//    insertWord(root, "three");
+/*
+ Given a binary search tree, print out
+ its data elements in increasing
+ sorted order.
+*/
+void printTree(Node *node) {
+    if (node == NULL) return;
+
+    printTree(node->left);
+    printf("\n%s: %d", node->word, node->count);
+    printTree(node->right);
+}
+
+void deleteTree(Node *node) {
+    if (node == NULL) return;
+
+    deleteTree(node->left);
+    deleteTree(node->right);
+    free(node->word);
+    node->word = NULL;
+    free(node);
+    node = NULL;
+}
+
+Node *buildTree( Node *node1) {
+    char word[22];
+    unsigned int i = 0;
+    char letter;
 
 
-//
-    char word[20];
-    char c;
-    FILE *input;
-    input = fopen("/home/yushitosh/CLionProjects/msl-clang-001/input01.txt", "r");
-
-    if (input == NULL) {
-        printf("Error!");
+    FILE *file = fopen("/home/yushitosh/CLionProjects/msl-clang-001/input01.txt", "r");
+    if (file == NULL) {
+        printf("Error! File cannot be opened");
         exit(1);
     }
 
 
-    int i = 0;
-    int wLengh = 0;
-    while (1) {
-        c = fgetc(input);
-        switch (c) {
-            case EOF:
-                break;
+
+    while (fgetc(file)!=EOF) {
+        letter = fgetc(file);
+        switch (letter) {
             case 32: {
-                char s[i+1];
-                strncpy(s, word, i);
-                i=0;
+                char temp[i + 1];
+                int size = i;
+                strncpy(temp, word, size);
+                temp[size] = '\n';
+                if (node1 != 0)
+                    insertWord(node1, temp);
+                else
+                    node1 = createLeaf(temp);
+
+                i = 0;
                 break;
             }
             default: {
-                word[i] = c;
+                word[i] = letter;
                 ++i;
             }
         }
-
     }
-    fclose(input);
+    fclose(file);
+}
+
+int main(int argc, char **argv) {
+
+//    Node *root = createLeaf("one");
+//    insertWord(root, "go");
+//    insertWord(root, "two");
+//    insertWord(root, "three");
+//    insertWord(root, "two");
+//    insertWord(root, "three");
+
+//    deleteTree(root);
+    Node *root = 0;
+
+
+    buildTree(root);
 
 
     exit(0);
